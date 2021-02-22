ba4ac0789cf8532ae2c750cd401a2a99f238c25d
Jaime Dantas <jaimejales@hotmail.com>
2021-02-22 20:43:00 -0300

    added top 10

--- script/main.js
+++ script/main.js
@@ -9,9 +9,15 @@ function startNetwork(data) {
     const container = document.getElementById("mynetwork");
 
     var options = {
+        physics: {
+            barnesHut: {
+                springLength: 100, 
+                springConstant: 0.04
+            },
+        },
         groups: {
             app: {
-                shape: "circle",
+                shape: "dot",
             },
             author: {
                 shape: "icon",
@@ -32,56 +38,152 @@ function startNetwork(data) {
  * In this example we do not mutate nodes or edges source data.
  */
 const nodes = new vis.DataSet([
-    { id: 1, label: "Google Photos", group: "app", value: 200},
-    { id: 2, label: "Facebook", group: "app", value: 500},
-    { id: 3, label: "Jaime", group: "author" },
-    { id: 4, label: "Alex", group: "author" },
-    { id: 5, label: "Arun", group: "author" },
-    { id: 6, label: "Twitter", group: "app" , value: 100},
-    { id: 7, label: "Mister", group: "author"},
-    { id: 8, label: "Prof", group: "author"},
+    { id: 1, label: "Google Photos", group: "app", value: 600},
+    { id: 2, label: "Google Duo - High Quality Video Calls", group: "app", value: 280},
+    { id: 3, label: "Candy Crush Soda Saga", group: "app",value: 200 },
+    { id: 4, label: "Google Play Music", group: "app",value: 180 },
+    { id: 5, label: "Candy Crush Saga", group: "app", value: 150 },
+    { id: 6, label: "Mini Militia - Doodle Army 2", group: "app" , value: 130},
+    { id: 7, label: "Candy Crush Jelly Saga", group: "app", value: 110},
+    { id: 8, label: "Castle Clash: Heroes of the Empire US", group: "app", value: 100},
+    { id: 9, label: "MX Player", group: "app", value: 100},
+    { id: 10, label: "Google Docs", group: "app",value: 100},
+    { id: 11, group: "author", label: "Lim Yen Ping"},
+    { id: 12, group: "author", label: "Emanuel Seuneke"},
+    { id: 13, group: "author", label: "Rhonda Paschal"},
+    { id: 14, group: "author", label: "Filipe Governa"},
+    { id: 15, group: "author", label: "Janko Kinčeš"},
+    { id: 16, group: "author", label: "Christina Reed"},
+    { id: 17, group: "author", label: "Josh Clark"},
+    { id: 18, group: "author", label: "Samuel Smith"},
+    { id: 19, group: "author", label: "Andri Untoro"},
+    { id: 20, group: "author", label: "William Howard"},
 ]);
 
 const edges = new vis.DataSet([
     {
         from: 1,
-        to: 3,
+        to: 11,
+        relation: "negative",
+        color: { color: "red" },
+    },
+    {
+        from: 1,
+        to: 16,
+        relation: "negative",
+        color: { color: "red" },
+    },
+    {
+        from: 3,
+        to: 17,
+        relation: "negative",
+        color: { color: "red" },
+    },
+    {
+        from: 5,
+        to: 19,
+        relation: "negative",
+        color: { color: "red" },
+    },
+    {
+        from: 11,
+        to: 10,
+        relation: "negative",
+        color: { color: "red" },
+    },
+    {
+        from: 1,
+        to: 14,
         relation: "negative",
         color: { color: "red" },
     },
     {
         from: 7,
-        to: 2,
+        to: 11,
         relation: "negative",
-        color: { color: "red" },
+        color: {color: "red" },
     },
     {
         from: 8,
-        to: 2,
+        to: 13,
         relation: "positive",
-        color: { color: "red" },
+        color: { color: "blue" },
     },
     {
         from: 2,
-        to: 3,
+        to: 15,
         relation: "positive",
         color: { color: "blue" },
     },
     {
         from: 5,
-        to: 2,
+        to: 20,
         relation: "neutral",
         color: { color: "black" },
     },
     {
         from: 4,
-        to: 1,
+        to: 11,
         relation: "positive",
         color: { color: "blue" },
     },
     {
         from: 4,
-        to: 2,
+        to: 18,
+        relation: "negative",
+        color: { color: "red" },
+    },
+    {
+        from: 5,
+        to: 16,
+        relation: "negative",
+        color: { color: "red" },
+    },
+       {
+        from: 1,
+        to: 18,
+        relation: "negative",
+        color: { color: "red" },
+    },
+    {
+        from: 8,
+        to: 12,
+        relation: "negative",
+        color: { color: "red" },
+    },
+    {
+        from: 9,
+        to: 12,
+        relation: "positive",
+        color: { color: "blue" },
+    },
+    {
+        from: 4,
+        to: 13,
+        relation: "positive",
+        color: { color: "blue" },
+    },
+    {
+        from: 2,
+        to: 20,
+        relation: "neutral",
+        color: { color: "black" },
+    },
+    {
+        from: 8,
+        to: 11,
+        relation: "positive",
+        color: { color: "blue" },
+    },
+    {
+        from: 1,
+        to: 17,
+        relation: "negative",
+        color: { color: "red" },
+    },
+    {
+        from: 6,
+        to: 11,
         relation: "negative",
         color: { color: "red" },
     },
